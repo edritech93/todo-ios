184764b155df95e2192c79a060838cc9a545bde4
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-02-03 15:34:16 +0700

    Initial Commit

---
+++ todo-ios/todo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_iosApp.swift
+//  todo-ios
+//
+//  Created by Yudi Edri Alviska on 03/02/21.
+//
+
+import SwiftUI
+
+@main
+struct todo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
